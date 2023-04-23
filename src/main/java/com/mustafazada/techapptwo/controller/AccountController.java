a89b4a22bacf71c19a32369ed03273c004121660
Ilkin <ilkinmustafazadeh@gmail.com>
2023-04-23 18:46:15 +0400

    add transfer ammoun account2account service

--- src/main/java/com/mustafazada/techapptwo/controller/AccountController.java
+++ src/main/java/com/mustafazada/techapptwo/controller/AccountController.java
@@ -1,12 +1,11 @@
 package com.mustafazada.techapptwo.controller;
 
+import com.mustafazada.techapptwo.dto.request.AccountToAccountRequestDTO;
 import com.mustafazada.techapptwo.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,7 +15,13 @@ public class AccountController {
     private AccountService accountService;
 
     @GetMapping("/account")
-    public ResponseEntity<?> account(){
+    public ResponseEntity<?> account() {
         return new ResponseEntity<>(accountService.getAccount(), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/transfer")
+    public ResponseEntity<?> amountTransfer(@RequestBody AccountToAccountRequestDTO accountToAccountRequestDTO) {
+        return new ResponseEntity<>(accountService.account2account(accountToAccountRequestDTO), HttpStatus.OK);
     }
 }
